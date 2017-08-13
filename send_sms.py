aae2f1b2a8362b4810c03d56670044198ebfbe9a
Brenna <bcmdarroch@gmail.com>
2017-08-12 19:32:17 -0700

    got send text working

---
+++ send_sms.py
@@ -0,0 +1,23 @@
+# /usr/bin/env python
+# download twilio-python library
+
+import os
+from twilio.rest import Client
+
+# test credentials
+account_sid = os.environ["TWILIO_SID_TEST"]
+auth_token = os.environ["TWILIO_TOKEN_TEST"]
+client = Client(account_sid, auth_token)
+
+message = client.api.account.messages.create(
+    to=os.environ["BEC_NUMBER"], from_=os.environ["TWILIO_NUMBER_TEST"], body="Testing brag log heeeey"
+)
+
+# live credentials
+# account_sid = os.environ["TWILIO_SID"]
+# auth_token = os.environ["TWILIO_TOKEN"]
+# client = Client(account_sid, auth_token)
+#
+# message = client.api.account.messages.create(
+#     to=os.environ["BEC_NUMBER"], from_=os.environ["TWILIO_NUMBER"], body="Testing brag log heeeey"
+# )
